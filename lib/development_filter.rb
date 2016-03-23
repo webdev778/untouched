be711d2c9db17bbf8a0a7714d18678a951a0476c
T.J. VanSlyke <teejay.vanslyke@gmail.com>
2016-03-23 15:38:39 -0700

    Refactors development filter code to be chainable

--- lib/development_filter.rb
+++ lib/development_filter.rb
@@ -6,25 +6,40 @@ class DevelopmentFilter
 
   attr_reader :params
 
+  class ResultSet
+
+    def initialize(collection=Unit.all)
+      @collection = collection
+    end
+
+    attr_reader :collection
+
+    def where_if(conditions, &block)
+      if block.call
+        ResultSet.new(collection.where(conditions))
+      else
+        ResultSet.new(collection)
+      end
+    end
+
+    def order(*args)
+      ResultSet.new(collection.order(*args))
+    end
+
+    def group(*args)
+      ResultSet.new(collection.group(*args))
+    end
+
+    def to_a
+      @collection.to_a
+    end
+  end
+
   def results
-    base = Unit.all
-
-    if params[:bedrooms]
-      base = base.
-        where(bedrooms: params[:bedrooms])
-    end
-
-    if params[:bathrooms]
-      base = base.
-        where(bathrooms: params[:bathrooms])
-    end
-
-    if params[:max_price].present?
-      base = base.
-        where(['price <= ?', params[:max_price]])
-    end
-
-    base.
+    ResultSet.new.
+      where_if(bedrooms: params[:bedrooms])          { params[:bedrooms] }.
+      where_if(bathrooms: params[:bathrooms])        { params[:bathrooms] }.
+      where_if(['price <= ?', params[:max_price]])  { params[:max_price].present? }.
       group('units.development_id, units.id').
       order('price ASC').
       to_a.uniq {|e| e.development.id}
--- spec/lib/development_filter_spec.rb
+++ spec/lib/development_filter_spec.rb
@@ -4,8 +4,8 @@ require 'development_filter'
 describe DevelopmentFilter do
   context "with no options" do
     subject { DevelopmentFilter.new }
-    let!(:development_1) { create(:development) }
-    let!(:development_2) { create(:development) }
+    let!(:development_1) { create(:development, photo: nil) }
+    let!(:development_2) { create(:development, photo: nil) }
 
     it "returns one item for each development" do
       expect(subject.results.length).to eq(2)
@@ -14,15 +14,14 @@ describe DevelopmentFilter do
     describe "the first development result" do
       let(:least_expensive_unit) { development_1.units.order('price ASC').first }
       it "returns the least expensive unit" do
-        p subject.results
-        expect(subject.results.first).to eq(least_expensive_unit)
+        expect(subject.results).to include(least_expensive_unit)
       end
     end
 
     describe "the second development result" do
       let(:least_expensive_unit) { development_2.units.order('price ASC').first }
       it "returns the least expensive unit" do
-        expect(subject.results.last).to eq(least_expensive_unit)
+        expect(subject.results).to include(least_expensive_unit)
       end
     end
   end
