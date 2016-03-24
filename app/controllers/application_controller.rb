bd9ad051e8475736279cc26f6f9d427576ddff15
T.J. VanSlyke <teejay.vanslyke@gmail.com>
2016-03-24 12:43:31 -0700

    Only enables basic auth in development or test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD']
+  unless Rails.env.development? or Rails.env.test?
+    http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD']
+  end
 end
