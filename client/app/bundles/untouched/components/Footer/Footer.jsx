9799af934fdfbc7eab39c5277d29e452315c123e
Alexey Elizarov <felex-ae@ya.ru>
2016-10-03 19:20:34 +0300

    footer capitalize copyright and add new link

--- client/app/bundles/untouched/components/Footer/Footer.jsx
+++ client/app/bundles/untouched/components/Footer/Footer.jsx
@@ -8,9 +8,10 @@ export default class Footer extends Component {
       <footer>
         <div className="container">
           <div className="copyright-info">
-            <span className="copyright-info__copyright">© 2016 Untouched</span>
+            <span className="copyright-info__copyright">© 2016 UNTOUCHED</span>
             <ul className="copyright-info__links">
               <li><Link to="/legal">Terms & Privacy</Link></li>
+              <li><Link to="https://consumer.vic.gov.au/duediligencechecklist" target="_blank">Due diligence</Link></li>
             </ul>
           </div>
         </div>
