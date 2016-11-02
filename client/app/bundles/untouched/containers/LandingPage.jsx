6327ac05813fce94fe05c82aaed04860bc2857fd
Alexey Elizarov <felex-ae@ya.ru>
2016-11-02 14:03:52 +0300

    changes on landing

--- client/app/bundles/untouched/containers/LandingPage.jsx
+++ client/app/bundles/untouched/containers/LandingPage.jsx
@@ -83,7 +83,7 @@ export default class LandingPage extends Component {
                 }}>
                   <div className="container">
                     <div className="landing__hero-intro">
-                      HELPS LEADING PROPERTY DEVELOPERS & PROJECT MARKETERS SELL PROJECTS FASTER & MORE EFFORTLESSLY
+                      TRUSTED BY LEADING PROPERTY DEVELOPERS AND PROJECT MARKETERS:
                     </div>
                     <Link className="landing__logo" to="/">
                       <img src="/images/logo-untouched-white.svg" alt="Untouched" />
@@ -94,37 +94,41 @@ export default class LandingPage extends Component {
             </Motion>
           </div>
 
-          <div className="landing__section landing__section-letter text-center">  
+          <div className="landing__section landing__section-letter">  
             <div className="container">
-              <div className="landing__hero">
+              <div className="landing__hero text-center">
                 Thoughts From Our Founder & CEO
               </div>
               <div className="landing__letter">
-                Hey, I’m Sasha Gilberg, the founder here at UNTOUCHED. 
+                Hey, I’m Sasha Gilberg, the founder here at UNTOUCHED.  
                 <br /><br />
-                I’m also selling apartment and townhouses developments all over Melbourne in my role as a Sales Consultant at CastranGilbert, one of Melbourne’s leading project sales & marketing agencies. 
+                I’m also selling new developments (primarily apartments and townhouses) all over Melbourne in my role as a Sales Consultant at CastranGilbert, one of Melbourne’s leading project sales and marketing agencies.
                 <br /><br />
-                From this vantage point, I’ve realised the most important things sales agents need to spend their time on is scheduling appointments with prospective purchasers, selling to them and bringing in new listing from developers.
+                Having worked across a wide variety of projects both large and small, I realised there were some repetitive processes that wasted a lot of my time, bottlenecking me from spending more time on lead generation, setting appointments and actually selling.
                 <br /><br />
-                But in the process of scheduling appointments and selling, there are some clunky manual processes that not only leave room for expensive errors, but significantly bottleneck their production capacity for number of appointments scheduled and number of sales presentations conducted in a given week. 
+                So I created a piece of software to try to automate some of the repetitive tasks I was doing so that I could be more efficient; and grow my sales volume.
                 <br /><br />
-                Furthermore, these clunky manual processes lengthen the sales cycle, increasing chances of losing the deal.
+                And fortunately, that piece of software worked tremendously well and was successful in taking my sales volume to the next level with far less effort.
                 <br /><br />
-                And as you grow your business and have more projects and more units to sell, that equals more stakeholders, emails, meetings, and interruptions. Stuff slips, communication suffers, everything turns ASAP. This chaos begins to feel like the new normal. But it’s not. Running around with your hair on fire isn’t normal.
+                But that was just the beginning. As I worked on more projects and had more units to sell, that equaled more stakeholders, emails, meetings, and interruptions. Stuff slipped, communication suffered, and everything turned ASAP. This chaos began to feel like the new normal. But it’s not normal. Running around with your hair on fire isn’t normal.
                 <br /><br />
-                Luckily there’s a better way: UNTOUCHED.
+                So I made a few tweaks and UNTOUCHED was born.
                 <br /><br />
-                I built UNTOUCHED because I was experiencing these same pains. And I wanted them gone! UNTOUCHED is a unique collection of tools and methods for managing your price lists, floorplans, presentation materials and channel partners, all baked into a single organized product. 
+                UNTOUCHED is a unique collection of tools and methods for managing your price lists, floorplans, presentation materials and channel partners, all baked into a single organized product.
                 <br /><br />
                 No more constantly updating your emails with new attachments as stock moves, no more uncertainty on exactly where a project is at and who sold what, and a much better handle on things. Out with chaos, in with order.
                 <br /><br />
-                Today, all the new development projects I work on run on UNTOUCHED. It enables me to do in seconds the tasks that used to take hours. 
+                All the problems and hassles I previously had to deal with on a daily basis just disappeared and I never had to think about them again. It was wonderful.
                 <br /><br />
-                But that’s not all, it’s also a powerful sales presentation tool that you can use in the office, on the phone or at the display centre to increase your enquiry to deal conversion rate.
+                Today, all the new development projects I work on run on UNTOUCHED. It enables me to do in seconds the tasks that used to take hours.
                 <br /><br />
-                As I’m still selling new developments and constantly using the tool, I’m relentlessly refining and improving UNTOUCHED nearly every day. There’s some other stuff out there, but there’s nothing else like UNTOUCHED.
+                I built UNTOUCHED for myself because I wanted to automate the repetitive tasks I was doing to have more time available to generate leads and do deals. But what it's evolved into is something that does so much more.
                 <br /><br />
-                To prove this to you, I’ll be hosting a LIVE webinar on <b>Friday 4th November at 10AM AEST</b> where I’ll show you exactly how I’ve used UNTOUCHED to take my sales to the next level with far less effort - and how agency principals and property developers themselves can enjoy even greater benefits.
+                It’s also a powerful sales presentation tool that you can use in the office, on the phone or at the display centre to increase your enquiry to deal conversion rate.
+                <br /><br />
+                As I’m still selling new developments and using UNTOUCHED every two seconds, I’m relentlessly refining and improving it nearly every day. There’s some other stuff out there, but there’s nothing else like UNTOUCHED.
+                <br /><br />
+                To prove this to you, I’ll be hosting a LIVE webinar on <a>Friday 11th November at 10AM AEST</a> where I’ll show you exactly how I’ve used UNTOUCHED to take my sales to the next level with far less effort - and how agency principals and property developers can use it to sell projects faster, and at the highest possible price.
                 <br /><br />
                 I’m 100% certain UNTOUCHED can improve how you manage and sell your new development projects. So please <a>register for the webinar</a> and I look forward to showing you how it all works.
                 <br /><br />
@@ -133,6 +137,7 @@ export default class LandingPage extends Component {
                 <br /><br />
                 Founder at UNTOUCHED / Sales Consultant at CastranGilbert
               </div>
+              <hr />
             </div>
           </div>
 
