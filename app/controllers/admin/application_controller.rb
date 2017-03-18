169b16945e4c751f76131f3f39cbde732b28cd3a
SuperRoR <isgloballove1981@gmail.com>
2017-03-18 11:17:02 -0400

    add new admin user

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -6,8 +6,23 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
-    unless Rails.env.development? or Rails.env.test?
-      http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD']
+    before_filter :authentication
+    def authentication
+      unless Rails.env.development? or Rails.env.test?
+        authenticate_or_request_with_http_basic('Admin') do |name, password|
+          if name == ENV['BASIC_ADMIN_USERNAME'] && password == ENV['BASIC_ADMIN_PASSWORD']
+            @role = "admin"
+            return true
+          elsif name == ENV['BASIC_USER_USERNAME1'] && password == ENV['BASIC_USER_PASSWORD1']
+            @role = "user"
+            return true
+          else
+            return false
+          end
+        end
+      else
+        @role = "admin"
+      end
     end
   end
 end
